3313550fba578b348852391d489ab045a830cbdc
cizquierdof <cizquierdof@gmail.com>
2020-06-17 10:31:21 +0200

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,39 +3,39 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { ServicesListComponent } from './components/services-list/services-list.component';
-import { SuppliersListComponent } from './components/suppliers-list/suppliers-list.component';
-import { HomeComponent } from './components/home/home.component';
-import { SearchComponent } from './components/search/search.component';
-import { NotFoundComponentComponent } from './components/not-found-component/not-found-component.component';
+import { EditarPerfilComponent } from './components/editar-perfil/editar-perfil.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { FormHomeComponent } from './components/form-home/form-home.component';
-import { UsersListComponent } from './components/users-list/users-list.component';
+import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { NgbdCarouselPauseComponent } from 'src/app/components/ngbd-carousel-pause/ngbd-carousel-pause.component';
+import { NotFoundComponentComponent } from './components/not-found-component/not-found-component.component';
 import { RegisterComponent } from './components/register/register.component';
-import { EditarPerfilComponent } from './components/editar-perfil/editar-perfil.component';
+import { SearchComponent } from './components/search/search.component';
 import { ServiceAddComponent } from './components/service-add/service-add.component';
+import { ServicesListComponent } from './components/services-list/services-list.component';
+import { SuppliersListComponent } from './components/suppliers-list/suppliers-list.component';
+import { UsersListComponent } from './components/users-list/users-list.component';
 
 import { UsersService } from './services/users.service';
 
-import {ButtonModule} from 'primeng/button';
-import {CardModule} from 'primeng/card';
-import {CarouselModule} from 'primeng/carousel';
-import {ContextMenuModule} from 'primeng/contextmenu';
-import {DataViewModule} from 'primeng/dataview';
-import {DialogModule} from 'primeng/dialog';
-import {DropdownModule} from 'primeng/dropdown';
-import {InputTextModule} from 'primeng/inputtext';
-import {ListboxModule} from 'primeng/listbox';
-import {GalleriaModule} from 'primeng/galleria';
-import {PanelMenuModule} from 'primeng/panelmenu';
-import {PanelModule} from 'primeng/panel';
-import {SidebarModule} from 'primeng/sidebar';
-import {TabMenuModule} from 'primeng/tabmenu';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { ContextMenuModule } from 'primeng/contextmenu';
+import { DataViewModule } from 'primeng/dataview';
+import { DialogModule } from 'primeng/dialog';
+import { DropdownModule } from 'primeng/dropdown';
+import { InputTextModule } from 'primeng/inputtext';
+import { ListboxModule } from 'primeng/listbox';
+import { GalleriaModule } from 'primeng/galleria';
+import { PanelMenuModule } from 'primeng/panelmenu';
+import { PanelModule } from 'primeng/panel';
+import { SidebarModule } from 'primeng/sidebar';
+import { TabMenuModule } from 'primeng/tabmenu';
 
 
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
@@ -57,6 +57,7 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
     ServiceAddComponent,
     SuppliersListComponent,
     UsersListComponent,
+    NgbdCarouselPauseComponent,
   ],
   imports: [
     AppRoutingModule,
@@ -64,7 +65,6 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
     BrowserModule,
     ButtonModule,
     CardModule,
-    CarouselModule,
     ContextMenuModule,
     DataViewModule,
     DialogModule,
